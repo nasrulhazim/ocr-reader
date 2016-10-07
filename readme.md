dd5516aa19fb9ecc19e0a57b4cc3d14dc392c3a7
Nasrul Hazim Bin Mohamad <nasrulhazim@nasrulhazim.local>
2016-10-07 22:17:53 +0800

    Added new clear Artisan command

--- readme.md
+++ readme.md
@@ -17,3 +17,9 @@ Go into the application `cd ocr-reader` and run `php artisan migrate`.
 Run `php artisan serve` to start using the application. You may register first before start using the reader.
 
 To run in terminal, you can use `php artisan ocr:read /path/to/image`
+
+**Others**
+
+Run `php artisan clear:cache` to clean up all cache and optimize class loader.
+
+Run `php artisan clear:serve` to clean up all cache, optimize class loader and serve the application instantly.
--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,15 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('clear:cache', function(){
+	$this->call('cache:clear');
+	$this->call('config:cache');
+	$this->call('view:clear');
+	$this->call('optimize');
+})->describe('Clear all cache and optimize class loader');
+
+Artisan::command('clear:serve', function(){
+	$this->call('clear:cache');
+	$this->call('serve');
+})->describe('Clear all cache, optimize class loader and serve the application');
