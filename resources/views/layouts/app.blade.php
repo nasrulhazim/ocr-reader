eb805e43963342b8181765fc511b14efab39424a
Nasrul Hazim Bin Mohamad <nasrulhazim@nasrulhazim.local>
2016-10-07 20:50:55 +0800

    Added Error Message Alert Box

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,7 +78,12 @@
                 </div>
             </div>
         </nav>
-
+        @if(Session::has('message-error'))
+            <div class="alert alert-danger">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <strong>Error!</strong> {{ Session::get('message-error') }}
+            </div>
+        @endif
         @yield('content')
     </div>
 
